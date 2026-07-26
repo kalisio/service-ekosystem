3a29e98638c8d98879dff1e3a4a4b2ed4db40633
cnouguier <ch.nouguier@gmail.com>
2026-07-26 23:55:27 +0200

    ci: try to fix coverage

--- vitest.base-config.js
+++ vitest.base-config.js
@@ -1,12 +1,7 @@
-import { fileURLToPath } from 'node:url'
-import path from 'node:path'
 import { defineConfig } from 'vitest/config'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export const baseConfig = defineConfig({
   test: {
-    globals: true,
     silent: false,
     testTimeout: 30000,
     coverage: {
